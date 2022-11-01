cb1ca2899cf229ec8aa7e91e7cbff66ec660d0ab
UnityBlackBeard <vladvasilevs@gmail.com>
2022-11-01 23:49:00 +0300

    task5 refactor switch

--- src/lesson_02/homework_03/Task_5.java
+++ src/lesson_02/homework_03/Task_5.java
@@ -15,24 +15,13 @@ public class Task_5 {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите номер месяца");
         String a = in.next();
-        int monthNumber=Integer.parseInt(a);
-        switch (monthNumber){
-            case 1: case 2: case 12:
-                System.out.println("Зима");
-                break;
-            case 3: case 4: case 5:
-                System.out.println("Весна");
-                break;
-            case 6: case 7: case 8:
-                System.out.println("Лето");
-                break;
-            case 9: case 10: case 11:
-                System.out.println("Осень");
-                break;
-            default:
+        switch (Integer.parseInt(a)){
+            case 1,2,12-> System.out.println("Зима");
+            case 3,4,5-> System.out.println("Весна");
+            case 6,7,8-> System.out.println("Лето");
+            case 9,10,11-> System.out.println("Осень");
+            default->
                 System.out.println("нет такого месяца");
-
-
         }
     }
 
